0162d71f22d7df6c388b33756aa171aea491f088
이현호 <ho971122@flow.team>
2026-09-10 21:29:04 +0900

    chore: 프로젝트 초기 설정

---
+++ ProductAssignment/ProductAssignment/ProductAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductAssignmentApp.swift
+//  ProductAssignment
+//
+//  Created by 이현호 on 9/10/26.
+//
+
+import SwiftUI
+
+@main
+struct ProductAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProductAssignment/ProductAssignmentTests/ProductAssignmentTests.swift
@@ -0,0 +1,19 @@
+//
+//  ProductAssignmentTests.swift
+//  ProductAssignmentTests
+//
+//  Created by 이현호 on 9/10/26.
+//
+
+import Testing
+@testable import ProductAssignment
+
+struct ProductAssignmentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
